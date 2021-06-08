51f09e95954c4026c3fa2ff44037f60182572c36
Soojung Kwak <skwak0205@gmail.com>
2021-06-08 09:43:29 +0900

    Memento Pattern

---
+++ memento_pattern/Memento.java
@@ -0,0 +1,13 @@
+package memento_pattern;
+
+public class Memento {
+    String state;
+
+    protected Memento(String state) {
+        this.state = state;
+    }
+
+    protected String getState() {
+        return this.state;
+    }
+}
---
+++ memento_pattern/Originator.java
@@ -0,0 +1,21 @@
+package memento_pattern;
+
+public class Originator {
+    String state;
+
+    public Memento createMemento() {
+        return new Memento(state);
+    }
+
+    public void restoreMement(Memento memento) {
+        this.state = memento.getState();
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+}
